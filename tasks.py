88bd173240dfa60488950645d301fc7f9d1efe34
Peter Hu <yesky93@sina.com>
2018-02-28 23:00:40 +0800

    test celery

---
+++ tasks.py
@@ -0,0 +1,12 @@
+# tasks.py
+import time
+from celery import Celery
+
+celery = Celery('tasks', broker='redis://localhost:6379/0')
+
+@celery.task
+def sendmail(mail):
+	print('send mail to %s' % mail['to'])
+	time.sleep(2.0)
+	print('sent')
+
